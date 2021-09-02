2760bd50cee269802c90b7303e96ea745b92d34c
Alexey Luzin <zdytch@gmail.com>
2021-09-02 14:08:00 +0300

    Use Bar schema in api_router

--- app/src/routers.py
+++ app/src/routers.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from schemas import Bar
 
 api_router = APIRouter()
 
-# @api_router.get('/history')
-# async def get_symbol_history(
-#     symbol: str, resolution: str, from_: int = Query(..., alias='from'), to: int = ...
-# ):
-#     if resolution == '1D':
-#         resolution = 'D'
-#     return await services.get_symbol_history(symbol, resolution, from_, to)
+
+@api_router.get('/history', response_model=list[Bar])
+async def get_symbol_history(
+    symbol: str, resolution: str, from_: int = Query(..., alias='from'), to: int = ...
+):
+    if resolution == '1D':  # TODO: Maybe useless?
+        resolution = 'D'
+
+    return await services.get_bars(symbol, resolution, from_, to)
