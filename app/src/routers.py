faeb0fcb2619aa938cad67d3c3a740d848d9bfc2
Alexey Luzin <zdytch@gmail.com>
2021-09-02 16:02:30 +0300

    Update routers.py

--- app/src/routers.py
+++ app/src/routers.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter, Query
 from schemas import Bar
+from ib_connector import IBConnector
+from datetime import datetime
+import pytz
 
 api_router = APIRouter()
+
+ibc = IBConnector()
 
 
 @api_router.get('/history', response_model=list[Bar])
 async def get_symbol_history(
     symbol: str, resolution: str, from_: int = Query(..., alias='from'), to: int = ...
 ):
-    if resolution == '1D':  # TODO: Maybe useless?
-        resolution = 'D'
+    from_dt = datetime.fromtimestamp(from_, pytz.utc)
+    to_dt = datetime.fromtimestamp(to, pytz.utc)
 
-    # return await services.get_bars(symbol, resolution, from_, to)
-    return [Bar(o=123.45, h=126.45, l=120.45, c=124.45, v=123456, t=1630540800)]
+    return await ibc.get_historical_bars(resolution, from_dt, to_dt)
