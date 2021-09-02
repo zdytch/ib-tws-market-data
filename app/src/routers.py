2ade0c84ac8b272aaa4ffc853e89a15ccbb5de9f
Alexey Luzin <zdytch@gmail.com>
2021-09-02 16:04:42 +0300

    Use Timeframe in argument

--- app/src/routers.py
+++ app/src/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Query
-from schemas import Bar
+from schemas import Bar, Timeframe
 from ib_connector import IBConnector
 from datetime import datetime
 import pytz
@@ -11,9 +11,12 @@ ibc = IBConnector()
 
 @api_router.get('/history', response_model=list[Bar])
 async def get_symbol_history(
-    symbol: str, resolution: str, from_: int = Query(..., alias='from'), to: int = ...
+    symbol: str,
+    timeframe: Timeframe,
+    from_: int = Query(..., alias='from'),
+    to: int = ...,
 ):
     from_dt = datetime.fromtimestamp(from_, pytz.utc)
     to_dt = datetime.fromtimestamp(to, pytz.utc)
 
-    return await ibc.get_historical_bars(resolution, from_dt, to_dt)
+    return await ibc.get_historical_bars(timeframe, from_dt, to_dt)
