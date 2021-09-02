123af55eb1b5535398d8aa78865d312e4cdf051c
Alexey Luzin <zdytch@gmail.com>
2021-09-02 14:45:25 +0300

    Add enums for Timeframe, InstrumentType

--- app/src/schemas.py
+++ app/src/schemas.py
@@ -1,5 +1,22 @@
 from pydantic import BaseModel
 from decimal import Decimal
+from enum import Enum
+
+
+class Timeframe(str, Enum):
+    M1 = '1'
+    M5 = '5'
+    M15 = '15'
+    M30 = '30'
+    M60 = '60'
+    DAY = '1D'
+    WEEK = '1W'
+    MONTH = '1M'
+
+
+class InstrumentType(str, Enum):
+    STOCK = 'STK'
+    FUTURE = 'FUT'
 
 
 class Bar(BaseModel):
