7156ad2857c5a93629ddf4d201618409a566d7b2
Alexey Luzin <zdytch@gmail.com>
2021-09-02 17:39:58 +0300

    Add get_symbol_info, get_config, convert_bar_list_to_chart_data

--- app/src/routers.py
+++ app/src/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Query
-from schemas import Bar, Timeframe, Exchange
+from schemas import Timeframe, Exchange, ChartData
 from ib_connector import IBConnector
 from datetime import datetime
 import services
@@ -10,10 +10,10 @@ api_router = APIRouter()
 ibc = IBConnector()
 
 
-@api_router.get('/history', response_model=list[Bar])
+@api_router.get('/history', response_model=ChartData)
 async def get_symbol_history(
-    ticker: str,
-    timeframe: Timeframe,
+    ticker: str = Query(..., alias='symbol'),
+    timeframe: Timeframe = Query(..., alias='resolution'),
     from_: int = Query(..., alias='from'),
     to: int = ...,
 ):
@@ -22,6 +22,18 @@ async def get_symbol_history(
     from_dt = datetime.fromtimestamp(from_, pytz.utc)
     to_dt = datetime.fromtimestamp(to, pytz.utc)
 
-    return await ibc.get_historical_bars(
+    bars = await ibc.get_historical_bars(
         symbol, exchange, instrument_type, timeframe, from_dt, to_dt
     )
+
+    return services.convert_bar_list_to_chart_data(bars)
+
+
+@api_router.get('/symbols')
+async def get_symbol_info(symbol: str):
+    return await services.get_symbol_info(symbol)
+
+
+@api_router.get('/config')
+def get_config():
+    return services.get_config()
--- app/src/services.py
+++ app/src/services.py
@@ -1,4 +1,4 @@
-from schemas import InstrumentType, Exchange
+from schemas import InstrumentType, Exchange, Bar, ChartData
 
 
 def get_instrument_type_by_exchange(exchange: Exchange) -> InstrumentType:
@@ -10,3 +10,55 @@ def get_instrument_type_by_exchange(exchange: Exchange) -> InstrumentType:
         raise ValueError(f'Cannot get instrument type, exchange unknown: {exchange}')
 
     return instrument_type
+
+
+async def get_symbol_info(ticker: str) -> dict:
+    exchange, symbol = tuple(ticker.split(':'))
+
+    # instrument = await instrument_services.get_instrument_by_ticker(ticker)
+    # instrument_type = chart_utils.convert_instrument_type_to_chart(instrument.type)
+    # session, timezone = chart_utils.get_session_and_timezone(instrument.exchange)
+    # price_scale = 10 ** abs(instrument.tick_size.normalize().as_tuple().exponent)
+    # min_movement = int(instrument.tick_size * price_scale)
+
+    return {
+        'name': ticker,
+        'ticker': ticker,
+        # 'type': instrument_type,
+        'description': 'Description',
+        # 'exchange': instrument.exchange,
+        # 'listed_exchange': instrument.exchange,
+        'session': '1700-1600',
+        'timezone': 'America/Chicago',
+        # 'currency_code': 'USD',
+        'has_daily': True,
+        'has_intraday': True,
+        'minmov': 1,
+        'pricescale': 100,
+    }
+
+
+def get_config() -> dict:
+    return {
+        'supported_resolutions': ['1', '5', '30', '1D'],
+        'supports_search': True,
+        'supports_group_request': False,
+        'supports_marks': False,
+        'supports_timescale_marks': False,
+    }
+
+
+def convert_bar_list_to_chart_data(bar_list: list[Bar]) -> ChartData:
+    # chart_data = {'t': [], 'h': [], 'l': [], 'o': [], 'c': [], 'v': [], 's': 'no_data'}
+    chart_data = ChartData()
+    for bar in bar_list:
+        chart_data.o.append(bar.o)
+        chart_data.h.append(bar.h)
+        chart_data.l.append(bar.l)
+        chart_data.c.append(bar.c)
+        chart_data.v.append(bar.v)
+        chart_data.t.append(bar.t)
+    if all(value for value in chart_data.dict().values()):
+        chart_data.s = 'ok'
+
+    return chart_data
