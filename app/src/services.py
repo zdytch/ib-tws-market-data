31b9c3ee0a0a5e7f3dee24628e8d3448ab35467a
Alexey Luzin <zdytch@gmail.com>
2021-09-03 09:53:03 +0300

    Add _get_historical_bars_from_ib()

--- app/src/services.py
+++ app/src/services.py
@@ -10,12 +10,10 @@ async def get_historical_bars(
     ticker: str, timeframe: Timeframe, from_ts: int, to_ts: int
 ) -> list[Bar]:
     exchange, symbol = tuple(ticker.split(':'))
-    instrument_type = _get_instrument_type_by_exchange(Exchange(exchange))
-    from_dt = datetime.fromtimestamp(from_ts, pytz.utc)
-    to_dt = datetime.fromtimestamp(to_ts, pytz.utc)
+    exchange = Exchange(exchange)
 
-    return await ibc.get_historical_bars(
-        symbol, exchange, instrument_type, timeframe, from_dt, to_dt
+    return await _get_historical_bars_from_ib(
+        symbol, exchange, timeframe, from_ts, to_ts
     )
 
 
@@ -33,6 +31,18 @@ def get_chart_data_from_bars(bar_list: list[Bar]) -> ChartData:
         chart_data.s = 'ok'
 
     return chart_data
+
+
+async def _get_historical_bars_from_ib(
+    symbol: str, exchange: Exchange, timeframe: Timeframe, from_ts: int, to_ts
+) -> list[Bar]:
+    instrument_type = _get_instrument_type_by_exchange(Exchange(exchange))
+    from_dt = datetime.fromtimestamp(from_ts, pytz.utc)
+    to_dt = datetime.fromtimestamp(to_ts, pytz.utc)
+
+    return await ibc.get_historical_bars(
+        symbol, exchange, instrument_type, timeframe, from_dt, to_dt
+    )
 
 
 def _get_instrument_type_by_exchange(exchange: Exchange) -> InstrumentType:
