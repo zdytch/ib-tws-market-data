4db9926f066a78383a6f3ceb711cc67253f95764
Alexey Luzin <zdytch@gmail.com>
2021-09-05 07:30:59 +0300

    Move cache logic to separate file

---
+++ app/src/cache.py
@@ -0,0 +1,89 @@
+from schemas import Instrument, Bar, Range
+from config.db import database
+from motor.motor_asyncio import AsyncIOMotorCollection as Collection
+from pymongo.errors import BulkWriteError
+
+
+async def get_bars(instrument: Instrument, range: Range) -> list[Bar]:
+    bars = []
+    collection, _ = _get_collections(instrument)
+
+    cursor = collection.find({'t': {'$gte': range.from_t, '$lte': range.to_t}}).sort(
+        't'
+    )
+    for mongo_bar in await cursor.to_list(999):
+        bar = Bar(**mongo_bar)
+        bars.append(bar)
+
+    return bars
+
+
+async def save_bars(instrument: Instrument, range: Range, bars: list[Bar]) -> None:
+    if bars:
+        collection, _ = _get_collections(instrument)
+
+        try:
+            await collection.insert_many([bar.dict() for bar in bars])
+        except BulkWriteError:
+            pass
+
+        await save_range(instrument, range)
+
+
+async def get_ranges(instrument: Instrument) -> list[Range]:
+    _, collection = _get_collections(instrument)
+    return [Range(**dic) for dic in await collection.find().to_list(999)]
+
+
+async def save_range(instrument: Instrument, range: Range) -> None:
+    _, collection = _get_collections(instrument)
+
+    await collection.insert_one(range.dict())
+
+    await _perform_range_defragmentation(collection)
+
+
+async def _perform_range_defragmentation(
+    collection: Collection,
+) -> None:
+    # TODO: Use mongo aggregation here
+    in_progress = True
+    while in_progress:
+        try:
+            ranges = [Range(**dic) for dic in await collection.find().to_list(999)]
+
+            for range in ranges:
+                for compare_range in ranges:
+                    if compare_range is not range and (
+                        range.from_t <= compare_range.from_t <= range.to_t
+                        or range.from_t <= compare_range.to_t <= range.to_t
+                    ):
+                        min_t = min((compare_range.from_t, range.from_t))
+                        max_t = max((compare_range.to_t, range.to_t))
+
+                        # TODO: Delete by id
+                        await collection.delete_many(
+                            {'from_t': {'$in': [compare_range.from_t, range.from_t]}}
+                        )
+                        await collection.insert_one({'from_t': min_t, 'to_t': max_t})
+
+                        raise StopIteration
+
+            in_progress = False
+
+        except StopIteration:
+            pass
+
+
+def _get_collections(instrument: Instrument) -> tuple[Collection, Collection]:
+    name_base = (
+        f'{instrument.symbol}_{instrument.exchange}_{instrument.timeframe}'.lower()
+    )
+    bar_col_name = f'{name_base}_bars'
+    range_col_name = f'{name_base}_ranges'
+
+    bar_collection = database[bar_col_name]
+    bar_collection.create_index('t', unique=True)
+    range_collection = database[range_col_name]
+
+    return bar_collection, range_collection
--- app/src/services.py
+++ app/src/services.py
@@ -9,10 +9,8 @@ from schemas import (
 )
 from ib_connector import IBConnector
 from datetime import datetime
-from config.db import database
-from motor.motor_asyncio import AsyncIOMotorCollection as Collection
-from pymongo.errors import BulkWriteError
 import pytz
+import cache
 
 ibc = IBConnector()
 
@@ -28,15 +26,15 @@ async def get_historical_bars(
     )
     range = Range(from_t=from_t, to_t=to_t)
 
-    cache_ranges = await _get_ranges_from_cache(instrument)
-    missing_ranges = _get_missing_ranges(range, cache_ranges)
+    cache_ranges = await cache.get_ranges(instrument)
+    missing_ranges = _calculate_missing_ranges(range, cache_ranges)
 
     for missing_range in missing_ranges:
         origin_bars = await _get_bars_from_ib(instrument, missing_range)
         if origin_bars:
-            await _save_bars_to_cache(instrument, missing_range, origin_bars)
+            await cache.save_bars(instrument, missing_range, origin_bars)
 
-    return await _get_bars_from_cache(instrument, range)
+    return await cache.get_bars(instrument, range)
 
 
 def get_chart_data_from_bars(bar_list: list[Bar]) -> ChartData:
@@ -55,39 +53,6 @@ def get_chart_data_from_bars(bar_list: list[Bar]) -> ChartData:
     return chart_data
 
 
-async def _get_bars_from_cache(instrument: Instrument, range: Range) -> list[Bar]:
-    bars = []
-    collection, _ = _get_collections(instrument)
-
-    cursor = collection.find({'t': {'$gte': range.from_t, '$lte': range.to_t}}).sort(
-        't'
-    )
-    for mongo_bar in await cursor.to_list(999):
-        bar = Bar(**mongo_bar)
-        bars.append(bar)
-
-    return bars
-
-
-async def _get_ranges_from_cache(instrument: Instrument) -> list[Range]:
-    _, collection = _get_collections(instrument)
-    return [Range(**dic) for dic in await collection.find().to_list(999)]
-
-
-async def _save_bars_to_cache(
-    instrument: Instrument, range: Range, bars: list[Bar]
-) -> None:
-    if bars:
-        collection, _ = _get_collections(instrument)
-
-        try:
-            await collection.insert_many([bar.dict() for bar in bars])
-        except BulkWriteError:
-            pass
-
-        await _save_range_to_cache(instrument, range)
-
-
 async def _get_bars_from_ib(instrument: Instrument, range: Range) -> list[Bar]:
     from_dt = datetime.fromtimestamp(range.from_t, pytz.utc)
     to_dt = datetime.fromtimestamp(range.to_t, pytz.utc)
@@ -95,7 +60,9 @@ async def _get_bars_from_ib(instrument: Instrument, range: Range) -> list[Bar]:
     return await ibc.get_historical_bars(instrument, from_dt, to_dt)
 
 
-def _get_missing_ranges(within_range: Range, existing_ranges: list[Range]) -> list:
+def _calculate_missing_ranges(
+    within_range: Range, existing_ranges: list[Range]
+) -> list:
     missing_ranges = []
     next_from_t = within_range.from_t
 
@@ -110,60 +77,6 @@ def _get_missing_ranges(within_range: Range, existing_ranges: list[Range]) -> li
         missing_ranges.append(Range(from_t=next_from_t, to_t=within_range.to_t))
 
     return missing_ranges
-
-
-async def _save_range_to_cache(instrument: Instrument, range: Range) -> None:
-    _, collection = _get_collections(instrument)
-
-    await collection.insert_one(range.dict())
-
-    await _perform_cached_range_defragmentation(collection)
-
-
-async def _perform_cached_range_defragmentation(
-    collection: Collection,
-) -> None:
-    # TODO: Use mongo aggregation here
-    in_progress = True
-    while in_progress:
-        try:
-            ranges = [Range(**dic) for dic in await collection.find().to_list(999)]
-
-            for range in ranges:
-                for compare_range in ranges:
-                    if compare_range is not range and (
-                        range.from_t <= compare_range.from_t <= range.to_t
-                        or range.from_t <= compare_range.to_t <= range.to_t
-                    ):
-                        min_t = min((compare_range.from_t, range.from_t))
-                        max_t = max((compare_range.to_t, range.to_t))
-
-                        # TODO: Delete by id
-                        await collection.delete_many(
-                            {'from_t': {'$in': [compare_range.from_t, range.from_t]}}
-                        )
-                        await collection.insert_one({'from_t': min_t, 'to_t': max_t})
-
-                        raise StopIteration
-
-            in_progress = False
-
-        except StopIteration:
-            pass
-
-
-def _get_collections(instrument: Instrument) -> tuple[Collection, Collection]:
-    name_base = (
-        f'{instrument.symbol}_{instrument.exchange}_{instrument.timeframe}'.lower()
-    )
-    bar_col_name = f'{name_base}_bars'
-    range_col_name = f'{name_base}_ranges'
-
-    bar_collection = database[bar_col_name]
-    bar_collection.create_index('t', unique=True)
-    range_collection = database[range_col_name]
-
-    return bar_collection, range_collection
 
 
 def _get_instrument_type_by_exchange(exchange: Exchange) -> InstrumentType:
