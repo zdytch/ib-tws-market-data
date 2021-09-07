34056e7655467ee2fc046b1a03b5325b5f00fe15
Alexey Luzin <zdytch@gmail.com>
2021-09-07 06:57:25 +0300

    Move instruments schemas

--- app/src/schemas.py
+++ app/src/schemas.py
@@ -1,14 +1,7 @@
 from pydantic import BaseModel
 from enum import Enum
 from datetime import datetime
-
-
-class Exchange(str, Enum):
-    NYSE = 'NYSE'
-    NASDAQ = 'NASDAQ'
-    GLOBEX = 'GLOBEX'
-    NYMEX = 'NYMEX'
-    ECBOT = 'ECBOT'
+from instruments.schemas import Instrument
 
 
 class Timeframe(str, Enum):
@@ -20,39 +13,6 @@ class Timeframe(str, Enum):
     DAY = 'D'
     WEEK = 'W'
     MONTH = 'M'
-
-
-class InstrumentType(str, Enum):
-    STOCK = 'STK'
-    FUTURE = 'FUT'
-
-
-class TradingSession(BaseModel):
-    open_t: int
-    close_t: int
-
-    def __str__(self):
-        return (
-            f'open_t={self.open_t}({datetime.fromtimestamp(self.open_t)}) '
-            f'close_t={self.close_t}({datetime.fromtimestamp(self.close_t)})'
-        )
-
-
-class Instrument(BaseModel):
-    symbol: str
-    exchange: Exchange
-    type: InstrumentType
-    description: str
-    tick_size: float
-    multiplier: float
-    nearest_session: TradingSession
-
-    def __str__(self):
-        return (
-            f'symbol={self.symbol} exchange={self.exchange} type={self.type} '
-            f'description={self.description} tick_size={self.tick_size} multiplier={self.multiplier} '
-            f'nearest_session={self.nearest_session}'
-        )
 
 
 class Bar(BaseModel):
