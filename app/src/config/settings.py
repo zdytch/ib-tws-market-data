5a64f13ea75fc412990cbcae0238d9b431b9dbc0
Alexey Luzin <zdytch@gmail.com>
2021-09-07 15:57:03 +0300

    Fix DB_URL

--- app/src/config/settings.py
+++ app/src/config/settings.py
@@ -8,7 +8,12 @@ API_URL_PREFIX = '/api/v1'
 
 SECRET_KEY = os.environ['APP_SECRET_KEY']
 
-DB_URL = f'mongodb://trixter-mdc-db:27017'
+DB_URL = (
+    f'postgresql://{os.getenv("POSTGRES_USER")}:'
+    f'{os.getenv("POSTGRES_PASSWORD")}@'
+    f'trixter-db:5432/'
+    f'{os.getenv("POSTGRES_DB")}'
+)
 
 BACKEND_CORS_ORIGINS = [
     'http://localhost:3000',
