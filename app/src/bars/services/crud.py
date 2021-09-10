2648bda1b52185694d50d9d7e727dc97f35728a0
Alexey Luzin <zdytch@gmail.com>
2021-09-10 14:54:02 +0300

    _perform_range_defragmentation(), _get_step_size()

--- app/src/bars/services/crud.py
+++ app/src/bars/services/crud.py
@@ -1,4 +1,4 @@
-from bars.models import BarLot, Bar, Range
+from bars.models import BarLot, Bar, Range, Timeframe
 from asyncpg.exceptions import UniqueViolationError
 
 
@@ -14,7 +14,7 @@ async def create_bars(bar_lot: BarLot, bars: list[Bar]) -> None:
 
         await Range.objects.create(bar_lot=bar_lot, from_t=min_t, to_t=max_t)
 
-        await _perform_range_defragmentation()
+        await _perform_range_defragmentation(bar_lot)
 
 
 async def get_bars(bar_lot: BarLot, range: Range) -> list[Bar]:
@@ -23,31 +23,59 @@ async def get_bars(bar_lot: BarLot, range: Range) -> list[Bar]:
     ).all()
 
 
-async def _perform_range_defragmentation() -> None:
-    pass
-    # in_progress = True
-    # while in_progress:
-    #     try:
-    #         ranges = [Range(**dic) for dic in await collection.find().to_list(999)]
+async def _perform_range_defragmentation(bar_lot: BarLot) -> None:
+    ranges = await Range.objects.filter(bar_lot=bar_lot).all()
+    step_size = _get_step_size(bar_lot.timeframe)
 
-    #         for range in ranges:
-    #             for compare_range in ranges:
-    #                 if compare_range is not range and (
-    #                     range.from_t <= compare_range.from_t <= range.to_t
-    #                     or range.from_t <= compare_range.to_t <= range.to_t
-    #                 ):
-    #                     min_t = min((compare_range.from_t, range.from_t))
-    #                     max_t = max((compare_range.to_t, range.to_t))
+    is_updated = False
+    for range in ranges:
+        for next_range in ranges:
+            if (
+                range.from_t
+                and range.to_t
+                and next_range.from_t
+                and next_range.to_t
+                and next_range is not range
+                and (
+                    range.from_t - step_size
+                    <= next_range.from_t
+                    <= range.to_t + step_size
+                    or range.from_t - step_size
+                    <= next_range.to_t
+                    <= range.to_t + step_size
+                )
+            ):
+                range.from_t = min((next_range.from_t, range.from_t))
+                range.to_t = max((next_range.to_t, range.to_t))
+                next_range.from_t = 0
+                next_range.to_t = 0
+                is_updated = True
 
-    #                     # TODO: Delete by id
-    #                     await collection.delete_many(
-    #                         {'from_t': {'$in': [compare_range.from_t, range.from_t]}}
-    #                     )
-    #                     await collection.insert_one({'from_t': min_t, 'to_t': max_t})
+    if is_updated:
+        async with Range.Meta.database.transaction():
+            for range in ranges:
+                if range.from_t and range.to_t:
+                    await range.update(['from_t', 'to_t'])
+                else:
+                    await range.delete()
 
-    #                     raise StopIteration
 
-    #         in_progress = False
+def _get_step_size(timeframe: Timeframe):
+    if timeframe == Timeframe.M1:
+        step_size = 60
+    elif timeframe == Timeframe.M5:
+        step_size = 60 * 5
+    elif timeframe == Timeframe.M30:
+        step_size = 60 * 30
+    elif timeframe == Timeframe.M60:
+        step_size = 60 * 60
+    elif timeframe == Timeframe.DAY:
+        step_size = 60 * 60 * 24
+    elif timeframe == Timeframe.WEEK:
+        step_size = 60 * 60 * 24 * 7
+    elif timeframe == Timeframe.MONTH:
+        step_size = 60 * 60 * 24 * 7 * 4
+    else:
+        raise ValueError(f'Cannot get step size for timeframe {timeframe}')
 
-    #     except StopIteration:
-    #         pass
+    return step_size
