5c7dd0b366a3ff8138d551d1d09600a1bfe36fec
Alexey Luzin <zdytch@gmail.com>
2021-09-13 08:28:54 +0300

    Handle live bar

--- app/src/bars/services/logic.py
+++ app/src/bars/services/logic.py
@@ -17,31 +17,43 @@ async def get_bars(bar_set: BarSet, range: Range) -> list[Bar]:
     existing_ranges = await BarRange.objects.filter(bar_set=bar_set).all()
     missing_ranges = _calculate_missing_ranges(range, existing_ranges)
     instrument = bar_set.instrument
+    live_bar = None
 
     for missing_range in missing_ranges:
-        # If missing range doesn't overlap with open session range
-        if not await instrument_services.is_overlap_open_session(
+        is_overlap_session = await instrument_services.is_overlap_open_session(
             instrument, missing_range
-        ):
+        )
+
+        # If missing range doesn't overlap with open session range
+        if not is_overlap_session:
             # Extend missing range by 1 day to overlap possible gaps in db
             missing_range.from_t -= 86400
             missing_range.to_t += 86400
 
         logger.debug(
-            f'Missing bars in range. Retreiving from origin... Instrument: {instrument}. Range: {missing_range}'
+            f'Missing bars in range. Retreiving from origin... '
+            f'{instrument.exchange}:{instrument.symbol}, {bar_set.timeframe}, {missing_range}'
         )
 
         try:
             origin_bars = await _get_bars_from_origin(bar_set, missing_range)
+
+            if (
+                is_overlap_session
+                and origin_bars
+                and await get_latest_timestamp(bar_set) < origin_bars[-1].t
+            ):
+                live_bar = origin_bars[-1]
+                origin_bars.remove(live_bar)
+
             await crud.add_bars(bar_set, origin_bars)
 
-            logger.debug(
-                f'Bars created. Instrument: {instrument}. Range: {missing_range}'
-            )
         except Exception as e:
             logger.debug(e)
 
     bars = await crud.get_bars(bar_set, range)
+    if live_bar:
+        bars.append(live_bar)
 
     return bars
 
@@ -56,10 +68,14 @@ async def _get_bars_from_origin(bar_set: BarSet, range: Range) -> list[Bar]:
 
     if bars:
         logger.debug(
-            f'Received bars from origin. Instrument: {instrument}. Range: {range}'
+            f'Received bars from origin. '
+            f'{instrument.exchange}:{instrument.symbol}, {bar_set.timeframe}, {range}'
         )
     else:
-        logger.debug(f'No bars from origin. Instrument: {instrument}. Range: {range}')
+        logger.debug(
+            f'No bars from origin. '
+            f'{instrument.exchange}:{instrument.symbol}, {bar_set.timeframe}, {range}'
+        )
 
     return bars
 
