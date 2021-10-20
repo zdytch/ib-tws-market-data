8e49d0f5858e2243a67f530abc5e0a197c370d80
Alexey Luzin <zdytch@gmail.com>
2021-10-20 20:30:42 +0300

    Add create(), get(), get_or_create(), update()

--- app/src/common/repositories.py
+++ app/src/common/repositories.py
@@ -1,17 +1,67 @@
 from config.db import async_session, Base
+from sqlalchemy import insert, update
 from sqlalchemy.future import select
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class BaseRepository:
+    NoResult = NoResultFound
+
     def __init__(self, model_class: Base) -> None:
         self._model_class = model_class
         self._session_factory = async_session
 
-    async def filter(self, **kwargs) -> list[Base]:
+    async def create(self, **kwargs) -> Base:
         async with self._session_factory() as session:
             async with session.begin():
-                st = await session.execute(
+                instance = self._model_class(**kwargs)
+                session.add(instance)
+                await session.commit()
+
+                instance = await self.get(id=instance.id)
+
+                return instance
+
+    async def get(self, **kwargs) -> Base:
+        async with self._session_factory() as session:
+            async with session.begin():
+                result = await session.execute(
                     select(self._model_class).filter_by(**kwargs)
                 )
 
-                return st.scalars().all()
+                return result.scalar_one()
+
+    async def get_or_create(self, **kwargs) -> Base:
+        async with self._session_factory() as session:
+            async with session.begin():
+                try:
+                    instance = await self.get(**kwargs)
+                    is_created = False
+
+                except self.NoResult:
+                    instance = await self.create(**kwargs)
+                    is_created = True
+
+            return instance, is_created
+
+    async def update(self, instance: Base, **kwargs) -> Base:
+        async with self._session_factory() as session:
+            async with session.begin():
+                await session.execute(
+                    update(self._model_class)
+                    .where(self._model_class.id == instance.id)
+                    .values(**kwargs)
+                )
+
+                instance = await self.get(id=instance.id)
+
+                return instance
+
+    async def filter(self, **kwargs) -> list[Base]:
+        async with self._session_factory() as session:
+            async with session.begin():
+                result = await session.execute(
+                    select(self._model_class).filter_by(**kwargs)
+                )
+
+                return result.scalars().all()
