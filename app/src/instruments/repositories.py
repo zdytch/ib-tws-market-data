0c7ce415f5adf767b59d30263020e843b4a5044c
Alexey Luzin <zdytch@gmail.com>
2021-10-20 20:31:35 +0300

    Add search_by_symbol_and_type()

--- app/src/instruments/repositories.py
+++ app/src/instruments/repositories.py
@@ -1,5 +1,26 @@
+from typing import Optional
 from common.repositories import BaseRepository
-from .models import Instrument, TradingSession
+from sqlalchemy.future import select
+from .models import Instrument, InstrumentType, TradingSession
 
-instrument_repo = BaseRepository(Instrument)
+
+class InstrumentRepo(BaseRepository):
+    async def search_by_symbol_and_type(
+        self, symbol: Optional[str], type: Optional[InstrumentType]
+    ) -> list[Instrument]:
+        async with self._session_factory() as session:
+            async with session.begin():
+                query = select(self._model_class)
+
+                if symbol:
+                    query = query.where(self._model_class.symbol.contains(symbol))
+                if type:
+                    query = query.filter_by(type=type)
+
+            result = await session.execute(query)
+
+            return result.scalars().all()
+
+
+instrument_repo = InstrumentRepo(Instrument)
 trading_session_repo = BaseRepository(TradingSession)
