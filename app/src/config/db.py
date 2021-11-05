4fc99b3dec1f2652504847e39cd2a20efe4538f0
Alexey Luzin <zdytch@gmail.com>
2021-11-05 05:52:10 +0300

    Use SQLModel in db config

--- app/src/config/db.py
+++ app/src/config/db.py
@@ -1,16 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlmodel import SQLModel
 from .settings import DB_URL
 
 engine = create_async_engine(DB_URL)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-Base = declarative_base()
-
 
 async def init_db():
     async with engine.begin() as conn:
         # TODO: Remove ignore
-        await conn.run_sync(Base.metadata.create_all)  # type: ignore
+        await conn.run_sync(SQLModel.metadata.create_all)  # type: ignore
