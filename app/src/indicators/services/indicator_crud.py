7b08e6c1c489a50ae95922a1e680b1ce3595701c
Alexey Luzin <zdytch@gmail.com>
2022-03-30 17:29:51 +0300

    Fix get_or_create_indicator()

--- app/src/indicators/services/indicator_crud.py
+++ app/src/indicators/services/indicator_crud.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 async def get_or_create_indicator(db: DB, bar_set: BarSet, length: int) -> Indicator:
     try:
-        query = select(Indicator).filter_by(bar_set=bar_set, length=length)
+        query = select(Indicator).filter_by(bar_set_id=bar_set.id, length=length)
         indicator = (await db.execute(query)).scalar_one()
 
     except NoResultFound:
