0d7bf119880762e37f6ee1482833d4207644e1b1
Alexey Luzin <zdytch@gmail.com>
2022-03-30 18:49:10 +0300

    Update routers.py

--- app/src/routers.py
+++ app/src/routers.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from charts.routers import chart_router
 from instruments.routers import instrument_router
 from indicators.routers import indicator_router
+from alerts.routers import alert_router
 
 api_router = APIRouter()
 
 api_router.include_router(chart_router, prefix='/charts')
 api_router.include_router(instrument_router, prefix='/instruments')
 api_router.include_router(indicator_router, prefix='/indicators')
+api_router.include_router(alert_router, prefix='/alerts')
