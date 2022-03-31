c123f7b7098118e9e57d5b3b07ad42cc9139a9de
Alexey Luzin <zdytch@gmail.com>
2022-03-31 13:30:49 +0300

    Join bar_set to indicator

--- app/src/indicators/services/indicator_crud.py
+++ app/src/indicators/services/indicator_crud.py
@@ -2,12 +2,17 @@ from indicators.models import Indicator
 from bars.models import BarSet
 from config.db import DB
 from sqlalchemy.future import select
+from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 
 
 async def get_or_create_indicator(db: DB, bar_set: BarSet, length: int) -> Indicator:
     try:
-        query = select(Indicator).filter_by(bar_set_id=bar_set.id, length=length)
+        query = (
+            select(Indicator)
+            .filter_by(bar_set_id=bar_set.id, length=length)
+            .options(joinedload(Indicator.bar_set))
+        )
         indicator = (await db.execute(query)).scalar_one()
 
     except NoResultFound:
