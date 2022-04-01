42b69eeaf887041bceb3f90312c906f6ea82ab2f
Alexey Luzin <zdytch@gmail.com>
2022-04-01 13:47:16 +0300

    Update indicator_crud.py

--- app/src/indicators/services/indicator_crud.py
+++ app/src/indicators/services/indicator_crud.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 async def get_or_create_indicator(db: DB, bar_set: BarSet, length: int) -> Indicator:
     try:
-        indicator = await get_indicator(db, bar_set, length)
+        indicator = await _get_indicator(db, bar_set, length)
 
     except NoResultFound:
         indicator = Indicator(bar_set_id=bar_set.id, length=length)
@@ -16,12 +16,12 @@ async def get_or_create_indicator(db: DB, bar_set: BarSet, length: int) -> Indic
 
         await db.commit()
 
-        indicator = await get_indicator(db, bar_set, length)
+        indicator = await _get_indicator(db, bar_set, length)
 
     return indicator
 
 
-async def get_indicator(db: DB, bar_set: BarSet, length: int) -> Indicator:
+async def _get_indicator(db: DB, bar_set: BarSet, length: int) -> Indicator:
     query = (
         select(Indicator)
         .filter_by(bar_set_id=bar_set.id, length=length)
